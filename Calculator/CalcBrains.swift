15406c0c86a2098bb38fbe3881e8cf3bb2068a26
TrAnd <devtrand@gmail.com>
2017-03-22 09:00:34 +0200

    iOS 10 course changes (L2)

--- Calculator/CalcBrains.swift
+++ Calculator/CalcBrains.swift
@@ -37,37 +37,37 @@ struct CalcBrains {
     private var operations: Dictionary<String,Operation> = [
         "π" :   .constant(Double.pi),    // M_PI
         "e" :   .constant(M_E),
-        "√" :   .unaryOp(sqrt, { "√(" + $0 + ")" }),
-        "cos" : .unaryOp(cos, { "cos(" + $0 + ")" }),
-        "sin" : .unaryOp(sin, { "sin(" + $0 + ")" }),
-        "tan" : .unaryOp(tan, { "tan(" + $0 + ")" }),
-        "ln" :  .unaryOp(log, { "ln(" + $0 + ")" }),
-        "+/-" : .unaryOp({ -$0 },{ "-(" + $0 + ")" }),
-        "x⁻¹" : .unaryOp({ 1.0/$0 }, { "1/(" + $0 + ")" }),
-        "x²" :  .unaryOp({ $0*$0 }, { "(" + $0 + ")²" }),
-        "eˣ" :  .unaryOp({ pow(M_E, $0) }, { "e^" + $0 }),
+        "√" :   .unaryOperation(sqrt, { "√(" + $0 + ")" }),
+        "cos" : .unaryOperation(cos, { "cos(" + $0 + ")" }),
+        "sin" : .unaryOperation(sin, { "sin(" + $0 + ")" }),
+        "tan" : .unaryOperation(tan, { "tan(" + $0 + ")" }),
+        "ln" :  .unaryOperation(log, { "ln(" + $0 + ")" }),
+        "+/-" : .unaryOperation({ -$0 },{ "-(" + $0 + ")" }),
+        "x⁻¹" : .unaryOperation({ 1.0/$0 }, { "1/(" + $0 + ")" }),
+        "x²" :  .unaryOperation({ $0*$0 }, { "(" + $0 + ")²" }),
+        "eˣ" :  .unaryOperation({ pow(M_E, $0) }, { "e^" + $0 }),
         //"×" : .binaryOp(multiply),
         //"×" : .binaryOp({ $0 * $1 }),
         //"÷" : .binaryOp({ $0 / $1 }),
         //"+" : .binaryOp({ $0 + $1 }),
         //"−" : .binaryOp({ $0 - $1 }),
-        "×" :   .binaryOp(*, { $0 + " × " + $1 }, 1),
-        "÷" :   .binaryOp(/, { $0 + " ÷ " + $1 }, 1),
-        "+" :   .binaryOp(+, { $0 + " + " + $1 }, 0),
-        "−" :   .binaryOp(-, { $0 + " - " + $1 }, 0),
-        "xʸ":   .binaryOp(pow, { $0 + " ^ " + $1 }, 2),
+        "×" :   .binaryOperation(*, { $0 + " × " + $1 }, 1),
+        "÷" :   .binaryOperation(/, { $0 + " ÷ " + $1 }, 1),
+        "+" :   .binaryOperation(+, { $0 + " + " + $1 }, 0),
+        "−" :   .binaryOperation(-, { $0 + " - " + $1 }, 0),
+        "xʸ":   .binaryOperation(pow, { $0 + " ^ " + $1 }, 2),
         "=" :   .equals,
         "Rand": .random({ Double(arc4random()) / Double(UINT32_MAX) }),
         // not used
-        "asin" : .unaryOp(asin, { "asin(" + $0 + ")"}),
-        "acos" : .unaryOp(acos, { "acos(" + $0 + ")"}),
-        "atan" : .unaryOp(atan, { "atan(" + $0 + ")"})
+        "asin" : .unaryOperation(asin, { "asin(" + $0 + ")"}),
+        "acos" : .unaryOperation(acos, { "acos(" + $0 + ")"}),
+        "atan" : .unaryOperation(atan, { "atan(" + $0 + ")"})
     ]
         
     private enum Operation {
         case constant(Double)
-        case unaryOp((Double) -> Double, (String) -> String)
-        case binaryOp((Double,Double) -> Double, (String,String) -> String, Int)
+        case unaryOperation((Double) -> Double, (String) -> String)
+        case binaryOperation((Double,Double) -> Double, (String,String) -> String, Int)
         case equals
         case random(() -> Double)
     }
@@ -81,23 +81,25 @@ struct CalcBrains {
                 //if !isPartialResult { clear() }     // clear state when new expression starts
                 accumulator = value
                 descriptionAccumulator = symbol
-            case .unaryOp(let function, let descriptionFunction) :
+            case .unaryOperation(let function, let descriptionFunction) :
                 if let operand = accumulator {
                     accumulator = function(operand)
+                    descriptionAccumulator = descriptionFunction(descriptionAccumulator)
                 }
-                descriptionAccumulator = descriptionFunction(descriptionAccumulator)
-            case .binaryOp(let function, let descriptionFunction, let precedence) :
+            case .binaryOperation(let function, let descriptionFunction, let precedence) :
                 executePendingBinaryOperation()
-                if currentPrecedence < precedence {
-                    descriptionAccumulator = "(" + descriptionAccumulator + ")"
-                }
-                currentPrecedence = precedence
+
                 if accumulator != nil {
-                    pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator!,
+                    if currentPrecedence < precedence {
+                        descriptionAccumulator = "(" + descriptionAccumulator + ")"
+                    }
+                    currentPrecedence = precedence
+                    
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!,
                                                    descriptionFunction: descriptionFunction, descriptionOperand: descriptionAccumulator)
+                    accumulator = nil
+                    descriptionAccumulator = " "
                 }
-                // desc acc should contain last operand only
-                descriptionAccumulator = " "
             case .equals :
                 executePendingBinaryOperation()
             case .random(let random) :
@@ -108,27 +110,38 @@ struct CalcBrains {
     }
     
     private mutating func executePendingBinaryOperation() {
-        if pending != nil {
-            if accumulator != nil {
-                accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator!)
-            }
-            descriptionAccumulator = pending!.descriptionFunction(pending!.descriptionOperand, descriptionAccumulator)
-            pending = nil
+        if pendingBinaryOperation != nil, accumulator != nil {
+            //accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator!)
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            
+            //descriptionAccumulator = pending!.descriptionFunction(pending!.descriptionOperand, descriptionAccumulator)
+            descriptionAccumulator = pendingBinaryOperation!.performDescription(with: descriptionAccumulator)
+            
+            pendingBinaryOperation = nil
         }
     }
     
-    private var pending: PendingBinaryOperationInfo?
+    private var pendingBinaryOperation: PendingBinaryOperation?
     
-    private struct PendingBinaryOperationInfo {
-        var binaryFunction: (Double,Double) -> Double
-        var firstOperand: Double
-        var descriptionFunction: (String, String) -> String
-        var descriptionOperand: String
+    private struct PendingBinaryOperation {
+        let function: (Double,Double) -> Double
+        let firstOperand: Double
+
+        let descriptionFunction: (String, String) -> String
+        let descriptionOperand: String
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+
+        func performDescription(with secondOperand: String) -> String {
+            return descriptionFunction(descriptionOperand, secondOperand)
+        }
     }
     
     mutating func clear() {
-        accumulator = 0.0
-        pending = nil
+        accumulator = nil
+        pendingBinaryOperation = nil
         internalProgram.removeAll()
         descriptionAccumulator = " "
         currentPrecedence = Int.max
@@ -175,9 +188,9 @@ struct CalcBrains {
 
     // MARK: Description section
     private var currentPrecedence = Int.max
-    private var descriptionAccumulator = "0" {
+    private var descriptionAccumulator = " " {
         didSet {
-            if pending == nil {
+            if pendingBinaryOperation == nil {
                 currentPrecedence = Int.max
             }
         }
@@ -185,17 +198,17 @@ struct CalcBrains {
 
     var description: String {
         get {
-            if pending == nil {
+            if pendingBinaryOperation == nil {
                 return descriptionAccumulator
             } else {
-                return pending!.descriptionFunction(pending!.descriptionOperand,
-                                                    pending!.descriptionOperand != descriptionAccumulator ? descriptionAccumulator : "")
+                return pendingBinaryOperation!.descriptionFunction(pendingBinaryOperation!.descriptionOperand,
+                                                    pendingBinaryOperation!.descriptionOperand != descriptionAccumulator ? descriptionAccumulator : "")
             }
         }
     }
 
     var isPartialResult: Bool {
-        return pending != nil
+        return pendingBinaryOperation != nil
     }
     
 //    private func getNumberForDescription(_ number: Double) -> String {
